6817c01b004f5e61728d71e3bed461fffee9cbdb
victamins <140756898+victamins@users.noreply.github.com>
2023-10-24 17:52:04 -0400

    test things

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,21 @@ import TitleSVG from '@/components/TitleSVG'
 
 export default function Home() {
   return (
-    <main>
+ 
+  
+   <main>
+
       <div id='stars'></div>
       <div id='stars2'></div>
       <div id='stars3'></div>
       <TitleSVG/>
-      <p> hello everyone CAN YOU SEE THIS PLEASE PLEASE PLEASE </p>
-      
+      <p className ="text-gray-50">
+ hello everyone CAN YOU SEE THIS PLEASE PLEASE PLEASE </p>
+
+ <button className="bg-sky-500 hover:bg-sky-700 ...">
+  $500 yummy yummy 
+</button>
+
     </main>
   )
 }
