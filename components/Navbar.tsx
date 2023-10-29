7948ff097d4c87e8ba310ada4cb44140bac0bb4f
victamins <140756898+victamins@users.noreply.github.com>
2023-10-28 21:57:09 -0400

    drop shadow, changed colour of navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,9 @@ export default function Navbar () {
     // const links = ["About Us", "Apply", "Sponsors"]
 
     return (
-    <nav className= "bg-gray-800 flex justify-between items-center h-20 p-4">
+    <nav className= "bg-transparent flex justify-between items-center h-20 p-4">
         {/* Logo */}
-        <p className="text-white ml-4 font-sans">Astral Hacks </p>
+        <p className="text-white ml-4 font-sans drop-shadow-lg">Astral Hacks </p>
         <ul> </ul>
     </nav>
     );
