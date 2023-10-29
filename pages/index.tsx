afe864a362afb2ed8cf74d83283fd4eed0323939
victamins <140756898+victamins@users.noreply.github.com>
2023-10-28 20:40:10 -0400

    vic is trying out the vsc extenstion

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ export default function Home() {
       <div id='stars3'></div>
 
       <TitleSVG/>
+      <h1>
+        vsc extenstion push. 
+      </h1>
       <p className ="text-gray-50"> hello everyone CAN YOU SEE THIS PLEASE PLEASE PLEASE </p>
       
       <button className="bg-sky-500 hover:bg-sky-700"> $500 yummy yummy </button>
