39fb611477a7c7121bc47108491f76dbc2714447
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-11-01 11:03:15 -0400

    fixed background, setup fonts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,7 @@
 export default function Navbar () {
-    // const links = ["About Us", "Apply", "Sponsors"]
-
     return (
-        <nav className= "bg-gray-800 flex justify-between items-center h-20 p-4">
+        <nav className= "flex justify-between items-center h-20 p-4">
             {/* Logo */}
-
-    </nav>
+        </nav>
     );
 }
