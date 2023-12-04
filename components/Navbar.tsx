4272cace30798fc184f870279b1bff561c1e70fa
Brian Zhang <101023105+Be4nis@users.noreply.github.com>
2023-12-03 20:15:28 -0500

    Changed for my version
    Commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,11 @@
-import { Major_Mono_Display } from 'next/font/google';
+import { Major_Mono_Display, Raleway } from 'next/font/google';
 import Link from 'next/link';
+import Image from 'next/image';
+const raleway = Raleway({
+    subsets: ['latin'],
+    variable: '--font-raleway',
+    weight: ['100',]
+})
 
 const majorMonoDisplay = Major_Mono_Display({
     subsets: ['latin'],
@@ -7,25 +13,29 @@ const majorMonoDisplay = Major_Mono_Display({
     weight: '400'
 })
 
-export default function Navbar () {
+
+export default function Navbar() {
     const links = [
-        { title: "Apply Now", route: "apply-now" }, 
+        { title: "Apply Now", route: "apply-now" },
         { title: "Event Info", route: "event-info" },
         { title: "FAQ", route: "faq" },
         { title: "Sponsors", route: "sponsor-info" },
+
     ]
-// Maybe name changes? Apply Now --> Apply, Sponsor Info --> Sponsors, 
-// Event Info --> About Us (?) maybe idk use judgement. 
+    // Maybe name changes? Apply Now --> Apply, Sponsor Info --> Sponsors, 
+    // Event Info --> About Us (?) maybe idk use judgement. 
     return (
-    <nav className= "flex justify-between items-center h-20 p-10">
-        {/* Logo Placeholder*/} <Link href="/" className="text-white font-sans drop-shadow-md">Astral Hacks </Link>
-        <ul className={`flex justify-start gap-6 list-none text-white drop-shadow-md`}> 
-            {links.map((link) => (
-                <li key={link.title}>
-                    <Link href={`/${link.route}`}>{link.title}</Link>
-                </li>
-            ))}
-        </ul>
-    </nav>
+        <nav className="flex justify-between items-center h-20 p-10">
+            <Link href="/" className={`${majorMonoDisplay.variable} font-heading text-white drop-shadow-md`}>
+                <Image src="/AstralHacksLogoWhite.png" alt="Logo" width={100} height={100} className="inline-flex" /> </Link>
+ 
+            <ul className={`${raleway.variable} font-text text-5xl flex justify-start gap-6 list-none text-white drop-shadow-md`}>
+                {links.map((link) => (
+                    <li key={link.title}>
+                        <Link href={`/${link.route}`}>{link.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 }
