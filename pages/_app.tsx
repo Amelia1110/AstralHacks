ba11de95dfbdfa289d9aabda838b12a0db73c0e5
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-12-09 21:21:08 -0500

    fixed index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,24 @@
+import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  const {pathname} = router
-    
   return (
-
     <>
       <Head>
         <title>Astral Hacks</title>
       </Head>
+        <header>
+          <Navbar/>
+        </header> 
         <main>
           <div id='stars'></div>
           <div id='stars2'></div>
           <div id='stars3'></div>
         </main>
 
-      <Component {...pageProps} />
+      <Component {...pageProps}/>
     </>
   )
 }
