dbf5ed5ae18a5bc253c829bea84990259289e1aa
victamins <140756898+victamins@users.noreply.github.com>
2023-12-10 21:04:44 -0500

    added login.ts (check please)

---
+++ pages/api/auth/login.ts
@@ -0,0 +1,31 @@
+import { connectToMongoDB } from "@/lib/mongodb"
+import User from "@/models/user"
+import { IUser } from "@/types"
+import { hash } from "bcryptjs"
+import mongoose from "mongoose"
+import { NextApiRequest, NextApiResponse } from "next"
+
+connectToMongoDB();
+
+export default async function handler(req: { method: string; body: { email: any; password: any } }, res: { status: (arg0: number) => { (): any; new(): any; end: { (): any; new(): any }; json: { (arg0: { message: string }): void; new(): any } } }) {
+  if (req.method !== 'POST') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || user.password !== password) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // TODO: Generate and send a token for authentication
+
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
