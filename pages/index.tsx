ef99709b3848c346abb2cd9e1522754fab41784f
Amelia1110 <ameliasongjiyun@hotmail.com>
2024-02-04 17:41:36 -0500

    landing page animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import TitleSVG from '@/components/TitleSVG';
-import { Major_Mono_Display, Raleway, Roboto_Mono } from 'next/font/google';
+import { motion } from 'framer-motion';
+import { Major_Mono_Display, Roboto_Mono } from 'next/font/google';
+import {SlArrowDown} from 'react-icons/sl'
 
 const majorMonoDisplay = Major_Mono_Display({
   subsets: ['latin'],
@@ -16,17 +18,27 @@ const robotoMono = Roboto_Mono({
 export default function Home() {
   return (
     <main>
-      <div className="grid justify-center">
+      <div className="flex flex-col justify-center items-center">
         <div className="flex flex-col pt-[calc(50vh_-_135px)] pb-64 items-center gap-8 ">
           <TitleSVG/>
-          <p className={`${robotoMono.variable} font-heading font-semibold tracking-wide text-white text-4xl`}>
-            MAY 17-19, 2024 | IN PERSON
-          </p>
+            <motion.div
+              initial={{ opacity: 0}}
+              animate={{ opacity: 1}}
+              transition={{ duration: 2.8, delay: 0.9 }}
+            >
+            <p className={`${robotoMono.variable} font-heading font-semibold tracking-wide text-white text-4xl`}>
+              MAY 17-19, 2024 | IN PERSON
+            </p>
+          </motion.div> 
         </div>
-        <p className={`${majorMonoDisplay.variable} font-styled text-center text-white text-xl`}>start your journey</p>
-        <svg className="w-14 h-14 text-white justify-self-center" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="0.4" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>
+        <motion.div className="flex flex-col gap-2 hover:gap-4 transition-all duration-300 w-fit items-center"
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          transition={{ duration: 2.5, delay: 1.5 }}
+        >
+          <p className={`${majorMonoDisplay.variable} font-styled text-center text-white text-xl`}>start your journey</p>
+          <SlArrowDown className="text-white text-2xl"/>
+        </motion.div>
       </div>
     </main>
   )
